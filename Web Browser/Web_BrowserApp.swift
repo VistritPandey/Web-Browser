16ce293b2727e067c0f9e7cb36275f93d864910d
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2022-08-21 17:22:58 -0400

    Initial Commit

---
+++ Web Browser/Web_BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Web_BrowserApp.swift
+//  Web Browser
+//
+//  Created by Vistrit Pandey on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Web_BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
